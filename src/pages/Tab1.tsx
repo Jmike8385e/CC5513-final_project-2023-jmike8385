229df87a4f3b9a329afd49b512d3dc025627c112
Jeremy Evans <jmike8385e@gmail.com>
2023-12-10 20:29:02 -0800

    working prototype

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel } from '@ionic/react';
+import React, {useState, useEffect} from 'react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  // dataset state variable to hold JSON data from WP
+  // to meet Ionic's requirement for typescript data types
+  // set type to <any[]> on next line
+  const [dataset, setDataset] = useState<any[]>([]);
+  // URL for WP JSON REST endpoint
+  const dataURL = "https://dev-cc5513-week11-2023-jmike8385e.pantheonsite.io/wp-json/twentytwentyone-child/v1/things";
+  // useEffect() to get JSON data and populate dataset state variable
+  useEffect(() => {
+    fetch(dataURL) // fetch() to load raw json string
+    .then(response => response.json()) // json() to convert raw string to json object
+    .then(data => setDataset(data)) // react state set function to populate data state var
+  },[])
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Things</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Things</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {/* use map() to loop through JSON array returned from WP */}
+        <IonList id="thing-list">
+          <IonListHeader>Things</IonListHeader>
+          {dataset.map((item, index) => (
+            <IonItem lines="inset" key={index}>
+              <IonLabel>
+                <h4>{item.post_title}</h4>
+                <p>{item.thing_description}</p>
+                <p>{item.thing_address}</p>
+                <img alt="My image" src={item.guid} />
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,25 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonListHeader, IonItem, IonLabel } from '@ionic/react';
+import React, {useState, useEffect} from 'react';
+import './Tab1.css';
 
-const Tab3: React.FC = () => {
+const Tab1: React.FC = () => {
+  // dataset state variable to hold JSON data from WP
+  // to meet Ionic's requirement for typescript data types
+  // set type to <any[]> on next line
+  const [dataset, setDataset] = useState<any[]>([]);
+  // URL for WP JSON REST endpoint
+  const dataURL = "https://dev-cc5513-week11-2023-jmike8385e.pantheonsite.io/wp-json/twentytwentyone-child/v1/specials";
+  // useEffect() to get JSON data and populate dataset state variable
+  useEffect(() => {
+    fetch(dataURL) // fetch() to load raw json string
+    .then(response => response.json()) // json() to convert raw string to json object
+    .then(data => setDataset(data)) // react state set function to populate data state var
+  },[])
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Things</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Things</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        {/* use map() to loop through JSON array returned from WP */}
+        <IonList id="thing-list">
+          <IonListHeader>Special</IonListHeader>
+          {dataset.map((item, index) => (
+            <IonItem lines="inset" key={index}>
+              <IonLabel>
+                <h4>{item.post_title}</h4>
+                <p>{item.special_description}</p>
+                <p>{item.special_name}</p>
+                <p>{item.special_number}</p>
+                <address>{item.special_email}</address>
+              </IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab3;
+export default Tab1;
